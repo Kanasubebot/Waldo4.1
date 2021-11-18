f0e6c2a58adbb406f5854d3417ac36e266e9a439
Juan Malalim <59202816+HairyPotah02@users.noreply.github.com>
2021-11-18 09:53:55 +0800

    Update callback.py

--- program/callback.py
+++ program/callback.py
@@ -32,7 +32,7 @@ async def cbstart(_, query: CallbackQuery):
                 ],
                 [InlineKeyboardButton("❓ Basic Guide", callback_data="cbhowtouse")],
                 [
-                    InlineKeyboardButton("📚 Commands", url=f"https://telegra.ph/𝙄𝙢𝙖𝙜𝙞𝙣𝙚𝙈𝙪𝙨𝙞𝙘𝘽𝙤𝙩-11-18"),
+                    InlineKeyboardButton("📚 Commands", url=f"https://telegra.ph/Waldo-Music-Bot-11-18"),
                     InlineKeyboardButton("❤ Donate", url=f"https://t.me/{OWNER_NAME}"),
                 ],
                 [
